f7cf20b8fbfb43cd50c339f9711b2786d7f18d96
Julian Hernando Henao Quintero <julian.henao@keralty.com.co>
2020-09-01 15:32:35 -0500

    Calculadora Básica

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,20 @@
+public class Calculadora {
+
+
+    public double suma(double n1, double n2) {
+        return n1+n2;
+    }
+
+    public  double resta(double n1, double n2) {
+        return n1-n2;
+    }
+
+    public double multiplicacion(double n1, double n2) {
+        return n1*n2;
+    }
+
+    public double division(double n1, double n2){
+        return (n2 != 0)? n1/n2 : 0;
+    }
+    
+}
---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,63 @@
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class CalculadoraTest extends TestCase {
+
+    Calculadora calculadora;
+
+    public void setUp() throws Exception {
+        calculadora = new Calculadora();
+    }
+
+    public void testSuma() {
+        // Arrange
+        double n1 = 0;
+        double n2 = 1;
+        double resultadoEsperado = 1;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.suma(n1,n2);
+
+        //Assert
+        Assert.assertEquals("La resta realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+    }
+
+    public void testResta() {
+        // Arrange
+        double n1 = 5;
+        double n2 = 1;
+        double resultadoEsperado = 4;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.resta(n1,n2);
+
+        //Assert
+        Assert.assertEquals("La resta realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+    }
+
+    public void testMultiplicacion() {
+        // Arrange
+        double n1 = 4;
+        double n2 = 5;
+        double resultadoEsperado = 20;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.multiplicacion(n1,n2);
+
+        //Assert
+        Assert.assertEquals("La multiplicación realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+    }
+
+    public void testDivision() {
+        // Arrange
+        double n1 = 3;
+        double n2 = 3;
+        double resultadoEsperado = 1;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.division(n1,n2);
+
+        //Assert
+        Assert.assertEquals("La division realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+    }
+}
