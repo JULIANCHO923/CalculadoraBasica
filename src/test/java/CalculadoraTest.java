4e5620819b555864d5cf562f13ab2fd8c2d18f64
Christian Gutierrez <ccgutierez@colsanitas.com>
2020-09-08 18:30:28 -0500

    Se agrega el metodo multiplicar
    Se agrega el metodo multiplicar

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,3 +1,4 @@
+
 import junit.framework.TestCase;
 import org.junit.Assert;
 
@@ -16,10 +17,34 @@ public class CalculadoraTest extends TestCase {
         double resultadoEsperado = 1;
         double deltaEsperado = 0;
         //Act
-        double resultadoActual = calculadora.suma(n1,n2);
+        double resultadoActual = calculadora.suma(n1, n2);
 
         //Assert
-        Assert.assertEquals("La resta realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+        Assert.assertEquals("La resta realizada no coincide: Se esperaba: " + resultadoEsperado + " resulto:" + resultadoActual, resultadoEsperado, resultadoActual, deltaEsperado);
     }
+
+    public void testMultiplicacion() {
+        // Arrange
+        double n1 = 4;
+        double n2 = 5;
+        double resultadoEsperado = 20;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.multiplicacion(n1, n2);
+        //Assert
+        Assert.assertEquals("La multiplicación realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+}
+public void testDivision() {
+        // Arrange
+        double n1 = 3;
+        double n2 = 3;
+        double resultadoEsperado = 1;
+        double deltaEsperado = 0;
+        //Act
+        double resultadoActual = calculadora.division(n1, n2);
+        //Assert
+        Assert.assertEquals("La division realizada no coincide: Se esperaba: "+resultadoEsperado + " resulto:" + resultadoActual,resultadoEsperado,resultadoActual,deltaEsperado);
+}
+
 
 }
